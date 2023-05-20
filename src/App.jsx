88daf755402b455ac3ddbfdf55b0c7c2571eab51
Amit258012 <amit258012@gmail.com>
2023-05-20 10:08:46 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import { HiHome } from "react-icons/hi";
+
+function App() {
+    return (
+        <>
+            <h1>App</h1>
+            <a href="#">
+                <HiHome />
+            </a>
+        </>
+    );
+}
+
+export default App;
