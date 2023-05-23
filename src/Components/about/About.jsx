c4b35e735480884a5aa9c3fcb4408e6b121d8416
Amit258012 <amit258012@gmail.com>
2023-05-23 17:11:44 +0530

    about section implemented

--- src/Components/about/About.jsx
+++ src/Components/about/About.jsx
@@ -1,11 +1,52 @@
+/* eslint-disable react/no-unescaped-entities */
 import "./about.css";
+import ME from "../../assets/me-about.jpg";
+import { FaAward } from "react-icons/fa";
+import { SiUdemy } from "react-icons/si";
+import { BiCodeAlt } from "react-icons/bi";
 
 function About() {
-    return (
-        <section id="about">
-            <h5>About</h5>
-        </section>
-    );
+  return (
+    <section id="about" className="about">
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
+      <div className="container about__container">
+        <div className="about__me">
+          <img src={ME} alt="amit's Image" className="about__me-image" />
+        </div>
+
+        <div className="about__content">
+          <div className="about__cards">
+            <article className="about__card">
+              <FaAward className="about__icon" />
+              <h5>Experience</h5>
+              <small>3+ months as Intern</small>
+            </article>
+            <article className="about__card">
+              <SiUdemy className="about__icon" />
+              <h5>Courses</h5>
+              <small>3+ Courses completed</small>
+            </article>
+            <article className="about__card">
+              <BiCodeAlt className="about__icon" />
+              <h5>Projects</h5>
+              <small>10+ Projects Completed</small>
+            </article>
+          </div>
+          <p className="para">
+            I'm Amit, a skilled front-end developer who loves creating
+            captivating digital experiences. Using HTML, CSS, JavaScript and
+            React, I bring static designs to life, making beautiful and
+            interactive websites. Let's work together to bring your vision to
+            reality with high-performance websites.
+          </p>
+          <a href="#contact" className="btn btn-primary">
+            Let's Talk
+          </a>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default About;
--- src/Components/header/Header.jsx
+++ src/Components/header/Header.jsx
@@ -5,30 +5,24 @@ import CTA from "./CTA";
 import HeaderSocial from "./HeaderSocials";
 
 function Header() {
-    return (
-        <header>
-            <div className="container header__container">
-                <h5>Hello I'm</h5>
-                <h1>Amit Jahagidar</h1>
-                <h5 className="text-light">Web Developer</h5>
-                <CTA />
-                <HeaderSocial />
+  return (
+    <header>
+      <div className="container header__container">
+        <h5>Hello I'm</h5>
+        <h1>Amit Jahagirdar</h1>
+        <h5 className="text-light">Web Developer</h5>
+        <CTA />
+        <HeaderSocial />
 
-                <div className="me">
-                    <img
-                        src={Me}
-                        alt="Amit Jahagiradr"
-                    />
-                </div>
-                <a
-                    href="#contact"
-                    className="scroll_down"
-                >
-                    Scroll Down
-                </a>
-            </div>
-        </header>
-    );
+        <div className="me">
+          <img src={Me} alt="Amit Jahagiradr" />
+        </div>
+        <a href="#contact" className="scroll_down">
+          Scroll Down
+        </a>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
